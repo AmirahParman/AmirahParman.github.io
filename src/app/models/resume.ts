1433a35b3e97badec664fd7b0edddf9fe3d68d7f
Amirah Parman <fthnamirah@hotmail.com>
2023-04-23 20:45:16 +0200

    refactored year calculation

--- src/app/models/resume.ts
+++ src/app/models/resume.ts
@@ -15,49 +15,42 @@ export class Experience {
 
     calculatedYears(): any {
         let monthsOfWorking = this.toDate.getMonth() - this.fromDate.getMonth() + (12 * (this.toDate.getFullYear() - this.fromDate.getFullYear()));
+        let excessMonthsFromYear = monthsOfWorking % 12;
 
-        //for 1 month
-        if (monthsOfWorking == 1) {
-            return monthsOfWorking + ' month';
+        let whenToShowMonths = excessMonthsFromYear != 0 && (monthsOfWorking > 1 || excessMonthsFromYear > 1);
+        let whenToShowMonth = excessMonthsFromYear != 0 && (monthsOfWorking == 1 || excessMonthsFromYear == 1);
+
+        let whenToShowYear = monthsOfWorking == 12 || (monthsOfWorking > 12 && excessMonthsFromYear > 0);
+        let whenToShowYears = monthsOfWorking > 12;
+
+        let result = '';
+        let yearsOfWorking = Math.round(monthsOfWorking / 12);
+
+        if(whenToShowYear) {
+            result += `${yearsOfWorking} year`;
         }
-        // for months
-        if (monthsOfWorking > 1 && monthsOfWorking <= 11) {
-            return monthsOfWorking + ' months';
-        }
-        //for 1 year
-        if (monthsOfWorking == 12) {
-            return Math.round(monthsOfWorking / 12) + ' year';
-        }
-        //for 1year with 1 month
-        if (monthsOfWorking == 13 && monthsOfWorking % 12 == 1) {
-            let yearsOfWorking = Math.round(monthsOfWorking / 12);
-            let monthsOfWorkingModulo = Math.round(monthsOfWorking % 12);
-            return yearsOfWorking + ' year ' + monthsOfWorkingModulo + ' month'
-        }
-        //for 1year with months
-        if (monthsOfWorking == 13 && monthsOfWorking % 12 > 1) {
-            let yearsOfWorking = Math.round(monthsOfWorking / 12);
-            let monthsOfWorkingModulo = Math.round(monthsOfWorking % 12);
-            return yearsOfWorking + ' year ' + monthsOfWorkingModulo + ' months'
-        }
-        //for years with no months
-        if (monthsOfWorking > 12 && monthsOfWorking % 12 == 0) {
-            let yearsOfWorking = Math.round(monthsOfWorking / 12);
-            return yearsOfWorking + ' years '
-        }
-        // for years & 1 month
-        if (monthsOfWorking > 12 && monthsOfWorking % 12 == 1) {
-            let yearsOfWorking = Math.round(monthsOfWorking / 12);
-            let monthsOfWorkingModulo = Math.round(monthsOfWorking % 12);
-            return yearsOfWorking + ' years ' + monthsOfWorkingModulo + ' month'
-        }
-        // for years & months
-        if (monthsOfWorking >= 13 && monthsOfWorking % 12 >= 1) {
-            let yearsOfWorking = Math.round(monthsOfWorking / 12);
-            let monthsOfWorkingModulo = Math.round(monthsOfWorking % 12);
-            return yearsOfWorking + ' years ' + monthsOfWorkingModulo + ' months'
+        else if(whenToShowYears) {
+            result += `${yearsOfWorking} years`;
         }
 
+        if(whenToShowMonth) {
+            if(monthsOfWorking == 1) {
+                result += ` ${monthsOfWorking} month`;
+            }
+            else {
+                result += ` ${excessMonthsFromYear} month`;
+            }
+        }
+        else if(whenToShowMonths) {
+            if(monthsOfWorking > 12) {
+                result += ` ${excessMonthsFromYear} months`;
+            }
+            else {
+                result += ` ${monthsOfWorking} months`;
+            }
+        }
+
+        return result;
     }
 }
 
