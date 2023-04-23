29974ab031b1e46dada7bfb863c1aea96c24528c
Amirah Parman <fthnamirah@hotmail.com>
2023-04-23 21:31:08 +0200

    Added Industry Labels

--- src/app/models/resume.ts
+++ src/app/models/resume.ts
@@ -4,14 +4,17 @@ export class Experience {
     positionHeld: string;
     company: string;
     responsibilities: string[];
+    industryLabels: string[];
 
-    constructor(fromDate: string, toDate: string, positionHeld: string, company: string, responsibilities: string[]) {
+    constructor(fromDate: string, toDate: string, positionHeld: string, company: string, responsibilities: string[], industryLabels: string[]) {
         this.fromDate = new Date(fromDate);;
         this.toDate = new Date(toDate);;
         this.positionHeld = positionHeld;
         this.company = company;
         this.responsibilities = responsibilities;
+        this.industryLabels = industryLabels;
     }
+
 
     calculatedYears(): any {
         let monthsOfWorking = this.toDate.getMonth() - this.fromDate.getMonth() + (12 * (this.toDate.getFullYear() - this.fromDate.getFullYear()));
@@ -26,23 +29,23 @@ export class Experience {
         let result = '';
         let yearsOfWorking = Math.round(monthsOfWorking / 12);
 
-        if(whenToShowYear) {
+        if (whenToShowYear) {
             result += `${yearsOfWorking} year`;
         }
-        else if(whenToShowYears) {
+        else if (whenToShowYears) {
             result += `${yearsOfWorking} years`;
         }
 
-        if(whenToShowMonth) {
-            if(monthsOfWorking == 1) {
+        if (whenToShowMonth) {
+            if (monthsOfWorking == 1) {
                 result += ` ${monthsOfWorking} month`;
             }
             else {
                 result += ` ${excessMonthsFromYear} month`;
             }
         }
-        else if(whenToShowMonths) {
-            if(monthsOfWorking > 12) {
+        else if (whenToShowMonths) {
+            if (monthsOfWorking > 12) {
                 result += ` ${excessMonthsFromYear} months`;
             }
             else {
