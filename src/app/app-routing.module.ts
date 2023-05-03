a159e0058d26ee82afa8e0a87f237461c45ff520
Amirah Parman <fthnamirah@hotmail.com>
2023-05-03 15:28:19 +0200

    anchor Job Description navigation and corrected resume navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { JobExperienceDescriptionComponent } from './tabs/job-experience-description/job-experience-description.component';
 import { ResumeComponent } from './tabs/resume/resume.component';
+
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled'
+};
 
 const routes: Routes = [
   { path: '', redirectTo: 'resume', pathMatch: 'full' }
@@ -10,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/models/job-description.ts
+++ src/app/models/job-description.ts
@@ -1,15 +1,56 @@
 export class jobDescription {
+    id: string;
     fromDate: Date;
     toDate: Date;
     company: string;
     description: string;
-    systems: string [];
+    systems: string[];
 
-    constructor(fromDate: string, toDate:string, company: string, description:string, systems: string[]){
+    constructor(id: string, fromDate: string, toDate: string, company: string, description: string, systems: string[]) {
+        this.id = id;
         this.fromDate = new Date(fromDate);
         this.toDate = new Date(toDate);
         this.company = company;
         this.description = description;
         this.systems = systems;
+    }
+    calculatedYears(): any {
+        let monthsOfWorking = this.toDate.getMonth() - this.fromDate.getMonth() + (12 * (this.toDate.getFullYear() - this.fromDate.getFullYear()));
+        let excessMonthsFromYear = monthsOfWorking % 12;
+
+        let whenToShowMonths = excessMonthsFromYear != 0 && (monthsOfWorking > 1 || excessMonthsFromYear > 1);
+        let whenToShowMonth = excessMonthsFromYear != 0 && (monthsOfWorking == 1 || excessMonthsFromYear == 1);
+
+        let whenToShowYear = monthsOfWorking == 12 || (monthsOfWorking > 12 && excessMonthsFromYear == 0);
+        let whenToShowYears = monthsOfWorking > 12;
+
+        let result = '';
+        let yearsOfWorking = Math.round(monthsOfWorking / 12);
+
+        if (whenToShowYear) {
+            result += `${yearsOfWorking} year`;
         }
+        else if (whenToShowYears) {
+            result += `${yearsOfWorking} years`;
+        }
+
+        if (whenToShowMonth) {
+            if (monthsOfWorking == 1) {
+                result += ` ${monthsOfWorking} month`;
+            }
+            else {
+                result += ` ${excessMonthsFromYear} month`;
+            }
+        }
+        else if (whenToShowMonths) {
+            if (monthsOfWorking > 12) {
+                result += ` ${excessMonthsFromYear} months`;
+            }
+            else {
+                result += ` ${monthsOfWorking} months`;
+            }
+        }
+
+        return result;
+    }
 }
